8ad8c79c865396b0e9ac2b17d4d06b671d439f7f
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-08 16:39:09 +0200

    Created role repo

---
+++ WebApi/Helpers/Repositories/RoleRepo.cs
@@ -0,0 +1,11 @@
+﻿using WebApi.Data;
+using WebApi.Models.Entities;
+
+namespace WebApi.Helpers.Repositories;
+
+public class RoleRepo : Repo<RoleEntity>
+{
+    public RoleRepo(DataContext context) : base(context)
+    {
+    }
+}

0f07ca8827a9acd987c3eb947f728f37d1aa55f3
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-08 16:53:39 +0200

    Added includes to role and group repo

--- WebApi/Helpers/Repositories/GroupRepo.cs
+++ WebApi/Helpers/Repositories/GroupRepo.cs
@@ -1,11 +1,32 @@
-﻿using WebApi.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Net;
+using WebApi.Data;
 using WebApi.Models.Entities;
+using WebApi.Models.Exceptions;
 
 namespace WebApi.Helpers.Repositories;
 
 public class GroupRepo : Repo<GroupEntity>
 {
+    private readonly DataContext _context;
+
     public GroupRepo(DataContext context) : base(context)
     {
+        _context = context;
+    }
+
+    public override async Task<GroupEntity?> GetAsync(Expression<Func<GroupEntity, bool>> predicate)
+    {
+        try
+        {
+            var entity = await _context.Groups.Include(x => x.Users).ThenInclude(x => x.User).FirstOrDefaultAsync(predicate);
+
+            return entity;
+        }
+        catch
+        {
+            throw new ApiException(HttpStatusCode.BadGateway, "An error occured when fetching the resource. Please try again.");
+        }
     }
 }

e4539e9f98cd1844b2f59ef5aceca81d00dc5bd3
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-08 16:43:01 +0200

    Created remaining repos

---
+++ WebApi/Helpers/Repositories/GroupRepo.cs
@@ -0,0 +1,11 @@
+﻿using WebApi.Data;
+using WebApi.Models.Entities;
+
+namespace WebApi.Helpers.Repositories;
+
+public class GroupRepo : Repo<GroupEntity>
+{
+    public GroupRepo(DataContext context) : base(context)
+    {
+    }
+}
---
+++ WebApi/Helpers/Repositories/UserGroupsRepo.cs
@@ -0,0 +1,11 @@
+﻿using WebApi.Data;
+using WebApi.Models.Entities;
+
+namespace WebApi.Helpers.Repositories;
+
+public class UserGroupsRepo : Repo<UserGroupsEntity>
+{
+    public UserGroupsRepo(DataContext context) : base(context)
+    {
+    }
+}

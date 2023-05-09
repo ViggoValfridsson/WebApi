b2dadaa273dbc8c6ef98e30017f15260a61a00ff
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-05-09 09:11:09 +0200

    Created dtos

---
+++ WebApi/Models/Dtos/GroupDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApi.Models.Dtos;
+
+public class GroupDto
+{
+    public string GroupName { get; set; } = null!;
+    public ICollection<UserDto> Users { get; set; } = new HashSet<UserDto>();
+}
